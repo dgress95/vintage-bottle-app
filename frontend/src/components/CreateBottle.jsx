8917af0255e39ff295d26b6e5a83c1d2eb13036f
Diane Gress <diane.gress@criticalmass.com>
2025-01-09 10:26:05 -0600

    Creates login and bottle form along with styling

--- frontend/src/components/CreateBottle.jsx
+++ frontend/src/components/CreateBottle.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import Nav from "./Nav";
 import axios from "../api/axios";
+import "../styles/create-bottle.css";
 
 const CreateBottle = () => {
   const [name, setName] = useState("");
@@ -25,6 +27,38 @@ const CreateBottle = () => {
     }
   };
 
-  // continue building out the form
-  return <form onSubmit={handleSubmit}></form>;
+  return (
+    <>
+      <Nav />
+      <form id="create-bottle" onSubmit={handleSubmit}>
+        <h2 className="heading">Add a Bottle to Your Collection</h2>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          placeholder="Bottle Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="location">Location:</label>
+        <input
+          type="text"
+          placeholder="Bottle Origin"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <label htmlFor="size">Size:</label>
+        <input
+          type="text"
+          placeholder="Bottle Size"
+          value={size}
+          onChange={(e) => setSize(e.target.value)}
+        />
+        <label htmlFor="file">Add a photo of the bottle:</label>
+        <input type="file" onChange={(e) => setPhoto(e.target.files[0])} />
+        <button type="submit">Add Bottle to Collection</button>
+      </form>
+    </>
+  );
 };
+
+export default CreateBottle;
